7c24772772c5ac5b52660c56a3c7f6ef5dbf7376
Ramkakde <kakderameshwar@gmail.com>
2021-02-22 13:39:44 +0530

    test
    test

---
+++ Employee.Model/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Model
+{ 
+	/// <summary>
+   /// Model Employee.
+   /// </summary>
+	public class Employee
+    {
+		/// <summary>
+		/// EmployeeId
+		/// </summary>
+		public int EmployeeId { get; set; }
+		/// <summary>
+		/// FirstName
+		/// </summary>
+		[Required]
+		[MaxLength(50)]
+		public string FirstName { get; set; }
+		/// <summary>
+		/// MiddleName
+		/// </summary>
+		[Required]
+		[MaxLength(50)]
+		public string MiddleName { get; set; }
+		/// <summary>
+		///  Surname
+		/// </summary>
+		[Required]
+		[MaxLength(40)]
+		public string Surname { get; set; }
+		/// <summary>
+		///  EmailAddress
+		/// </summary>
+		[Required]
+		[MaxLength(100)]
+		public string EmailAddress { get; set; }
+
+		/// <summary>
+		///  IsDeleted
+		/// </summary>
+		public bool IsDeleted { get; set; }
+
+		
+	}
+}
---
+++ Employee.Model/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Model
+{
+	/// <summary>
+	/// Model Employee.
+	/// </summary>
+	public class User
+	{
+		/// <summary>
+		/// UserId
+		/// </summary>
+		[Required]
+		[MaxLength(50)]
+		public string UserId { get; set; }
+		/// <summary>
+		/// Password
+		/// </summary>
+		[Required]
+		[MaxLength(50)]
+		public string Password { get; set; }
+
+		/// <summary>
+		/// UserRole
+		/// </summary> 
+		public string Role { get; set; }
+	}
+}
